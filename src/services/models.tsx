cd0de3d33a18fb15e9cd46ba34da354b6e925b1f
MeiriBernardina <meiri.umc@gmail.com>
2022-10-03 14:22:01 -0300

    respostas endpoints para login user products

---
+++ src/services/models.tsx
@@ -0,0 +1,30 @@
+export interface Login {
+  email: String;
+  password: String;
+}
+
+export interface User {
+  name: String;
+  password: String;
+  email: String;
+  experience: Number;
+}
+export interface Category {
+  name: String;
+  searchName: String;
+  product: Product[];
+}
+
+export interface Brand {
+  searchName: String;
+  brandName: String;
+  product: Product[];
+}
+
+export interface Product {
+  name: String;
+  price: Number;
+  brand: Brand[];
+  unity: Number;
+  category: Category[];
+}
