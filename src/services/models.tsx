97ffa5c51de27846a47aeb7bd437ac8f6a8ef525
Meiri Bernardina <51059393+MeiriBernardina@users.noreply.github.com>
2022-10-10 16:57:19 -0300

    respostas endpoints para login user products (#3)
    * respostas endpoints para login user products
    
    * refactor model token
    
    * refector: interfaces
    
    * refector: interfaces

---
+++ src/services/models.tsx
@@ -0,0 +1,91 @@
+import { DateLocale } from 'yup/lib/locale';
+
+export interface Address {
+  cep: String;
+  street: String;
+  number: String;
+  complement?: String;
+  district: String;
+  city: String;
+  state: String;
+}
+
+export interface Brand {
+  searchName?: String;
+  brandName: String;
+  product?: Product[];
+}
+
+export interface Category {
+  uuid?: String;
+  name: String;
+  searchName?: String;
+  product?: Product[];
+}
+
+export interface Market {
+  uuid?: String;
+  locateX: String;
+  locateY: String;
+  name: String;
+  description?: String;
+  logo?: String;
+  address?: Address;
+  schedules?: Schedule[];
+  products?: Product[];
+}
+
+export interface Product {
+  name: String;
+  searchName?: String;
+  price: Number;
+  brand: Brand;
+  category: Category;
+  unity: Number;
+  markets: Market[];
+}
+
+export interface ProductList {
+  uuid?: String;
+  shoppingList: ShoppingList;
+  product: Product;
+  quantity: Number;
+}
+
+export interface Schedule {
+  oppeningHour: DateLocale;
+  closingHour: DateLocale;
+  dayOfWeek: Number;
+  market: Market;
+}
+
+export interface ShoppingList {
+  uuid?: String;
+  user: User;
+  productList?: ProductList[];
+}
+
+export interface Token {
+  token: string;
+}
+
+export interface User {
+  uuid?: String;
+  name: String;
+  password: String;
+  email: String;
+  experience?: Number;
+  shoppingLists?: ShoppingList[];
+  permissions?: Permission[];
+  address?: Address;
+  tokens: RefreshToken[];
+}
+
+export interface UserLogin {
+  email: String;
+  senha: String;
+}
+
+export interface Permission {}
+
+export interface RefreshToken {}
