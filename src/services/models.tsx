58820a71bc2016dec361fb3a9f553fbdd7092d88
MeiriBernardina <meiri.umc@gmail.com>
2022-10-10 16:50:26 -0300

    refector: interfaces

--- src/services/models.tsx
+++ src/services/models.tsx
@@ -1,6 +1,71 @@
-export interface Login {
-  email: String;
-  password: String;
+import { DateLocale } from "yup/lib/locale";
+
+export interface Address {
+  cep: String;
+  street: String;
+  number: String;
+  complement?: String;
+  district: String;
+  city: String;
+  state: String;
+}
+
+export interface Brand {
+  searchName?: String;
+  brandName: String;
+  product?: Product[];
+}
+
+export interface Category {
+  uuid?: String;
+  name: String;
+  searchName?: String;
+  product?: Product[];
+}
+
+export interface Market {
+  uuid?: String;
+  locateX: String;
+  locateY: String;
+  name: String;
+  description?: String;
+  logo?: String;
+  address?: Address;
+  schedules?: Schedule[];
+  products?: Product[];
+
+}
+
+export interface Product {
+  name: String;
+  searchName?: String;
+  price: Number;
+  brand: Brand;
+  category: Category;
+  unity: Number;
+  markets: Market[];
+ 
+}
+
+export interface ProductList {
+  uuid?: String;
+  shoppingList: ShoppingList;
+  product: Product;
+  quantity: Number;
+}
+
+export interface Schedule {
+  oppeningHour: DateLocale;
+  closingHour: DateLocale;
+  dayOfWeek: Number;
+  market: Market;
+
+}
+
+export interface ShoppingList {
+  uuid?: String;
+  user: User;
+  productList?: ProductList[];
 }
 
 export interface Token {
@@ -8,27 +73,21 @@ export interface Token {
 }
 
 export interface User {
+  uuid?: String;
   name: String;
   password: String;
   email: String;
-  experience: Number;
-}
-export interface Category {
-  name: String;
-  searchName: String;
-  product: Product[];
-}
-
-export interface Brand {
-  searchName: String;
-  brandName: String;
-  product: Product[];
+  experience?: Number;
+  shoppingLists?: ShoppingList[];
+  permissions?: Permission[];
+  address?: Address;
+  tokens: RefreshToken[];
 }
 
-export interface Product {
-  name: String;
-  price: Number;
-  brand: Brand[];
-  unity: Number;
-  category: Category[];
+export interface Permission {
+
+}
+
+export interface RefreshToken {
+
 }
