53bdfd8cf80bdfa07e09dcc7844fac46c4b9dce4
AndrezaEmiliano <andreza.alamino@gmail.com>
2022-10-09 21:29:45 -0300

    chore: create button, external template, login, user and refactoring

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,59 @@
-import * as S from "./styles";
+import { ChangeEvent, useState } from "react";
+import ExternalAccessContainer from "../../templates/ExternalLayout";
+
+import * as S from "../styles";
+import InputText from "../../components/InputText";
+import login from "../../assets/login.png";
+import Button from "../../components/Button";
+import { signIn } from "../../services/auth";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { schema } from "../../utils/schema";
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = async (data: any) => {
+    console.log(data);
+    await signIn(data.email, data.password);
+  };
   return (
     <S.Wrapper>
-      <S.Text>Em construção</S.Text>
+      <ExternalAccessContainer title="Oi! Vamos começar?" image={login}>
+        <S.Form onSubmit={onSubmit}>
+          <InputText
+            {...register("email")}
+            name="email"
+            label="E-mail"
+            placeholder="Digite seu e-mail"
+            errorMessage={errors.email?.message}
+          />
+
+          <InputText
+            {...register("password")}
+            name="password"
+            label="Senha"
+            placeholder="Digite sua senha"
+            errorMessage={errors.password?.message}
+          />
+
+          <Button text="Entrar" onClick={handleSubmit(onSubmit)} />
+        </S.Form>
+        <S.Text>
+          Ainda não possui cadastro?
+          <S.Link>Cadastre-se</S.Link>
+        </S.Text>
+      </ExternalAccessContainer>
     </S.Wrapper>
   );
 }
--- src/services/auth.tsx
+++ src/services/auth.tsx
@@ -2,10 +2,15 @@ import { api } from "./api";
 import { CONTEXT_AUTH, SINGIN_ENDPOINT } from "../constants";
 
 export async function signIn(email: string, password: string) {
-  const { data } = await api.post(`${CONTEXT_AUTH}${SINGIN_ENDPOINT}`, {
+  console.log("chegou");
+  const { data } = await api.post(`auth/login `, {
     email,
     password,
   });
 
   return data;
 }
+
+// api-economarket.herokuapp.com/auth/login
+// email: manager@manager.com
+// password: @manage
