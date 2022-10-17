3d91d70708331a81d4184d894115fe89e772dd9a
Renan Braga <psico.bragarenan@gmail.com>
2022-10-17 08:49:10 -0300

    chore: put env

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: "https://api-economarket.herokuapp.com"
   // headers: {
   //   "Accept": "*/*",
   //   "Accept-Encoding": "gzip, deflate, br",
