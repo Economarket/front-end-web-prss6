c088b75e9a8ba0db5df0fb4691ca41ccfa8cb9c1
Renan Braga <psico.bragarenan@gmail.com>
2022-11-02 16:55:59 -0300

    feat: implementa toast exemplo

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -10,6 +10,9 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { schemaLogin } from "../../utils/schema";
 import InputPassword from "../../components/InputPassword";
 import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Login() {
   const navigate = useNavigate();
@@ -27,15 +30,39 @@ export default function Login() {
   });
 
   const onSubmit = async (data: any) => {
+    notify()
     await signIn(data.email, data.password).then((response) => {
       localStorage.setItem("token", response.access_token);
 
       if (localStorage.getItem("token")) {
         navigate("/");
       }
+      
     });
   };
+  
+  const toastId = React.useRef(null);
+  const customId = "custom-id-yes";
+
+  const notify = () => {
+    if(!toast.isActive(customId)) {
+      toast('Logando na aplicação...', {
+        toastId: customId,
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+    }
+    handleSubmit(onSubmit)
+  }
+
   return (
+    
     <S.Wrapper>
       <ExternalAccessContainer title="Que bom te ver novamente!" image={login}>
         <S.Form onSubmit={handleSubmit(onSubmit)}>
@@ -57,6 +84,7 @@ export default function Login() {
 
           <Button text="Entrar" onClick={handleSubmit(onSubmit)} />
         </S.Form>
+
 
         <S.Text>
           Ainda não possui cadastro?
