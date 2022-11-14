7da65a856c0453a0f1cd35d89067cbad36c9d14c
AndrezaEmiliano <andreza.alamino@gmail.com>
2022-11-13 22:22:54 -0300

    fix base_url

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "../constants";
 
 export const api = axios.create({
-  baseURL: BASE_URL
+  baseURL: "https://api-economarket.herokuapp.com/",
   // headers: {
   //   "Accept": "*/*",
   //   "Accept-Encoding": "gzip, deflate, br",
