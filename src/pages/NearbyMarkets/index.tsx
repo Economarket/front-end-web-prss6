7518467b5d2390c511c2d89571ca584d76c23f88
MeiriBernardina <meiri.umc@gmail.com>
2022-11-19 10:21:45 -0300

    mock products

--- src/pages/NearbyMarkets/index.tsx
+++ src/pages/NearbyMarkets/index.tsx
@@ -1,13 +1,5 @@
-import { yupResolver } from '@hookform/resolvers/yup';
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
-import IconChevronDown from '../../assets/icons/chevronDown';
-import Button from '../../components/Button';
-import InputText from '../../components/InputText';
-import { postProduct } from '../../services/product';
-import { postUser } from '../../services/user';
 import { Title } from '../../templates/InternalLayout/styles';
-import { schemaRegisterProduct } from '../../utils/schema';
+
 import * as S from '../styles';
 
 export default function NearbyMarkets() {
--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,3 +1,7 @@
+import IconSearch from '../../assets/icons/search';
+import InputText from '../../components/InputText';
+import ProductCard from '../../components/ProductCard';
+import { products_mock } from '../../mock/mock';
 import { Title } from '../../templates/InternalLayout/styles';
 import * as S from '../styles';
 
@@ -5,6 +9,24 @@ export default function Product() {
   return (
     <S.Wrapper>
       <Title> Produtos</Title>
+      <S.Form>
+        <InputText 
+          name="buscaProduto" 
+          placeholder="Nome do produto"
+          icon={<IconSearch />}
+          iconPosition={'left'} />
+      </S.Form>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          width: '100%',
+        }}
+      >
+        {products_mock.map((p) => (
+          <ProductCard product={p} onClick={() => console.log(p.name)} />
+        ))}
+      </div>
     </S.Wrapper>
   );
 }
