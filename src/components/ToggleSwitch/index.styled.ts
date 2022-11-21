64ef049e20903a0290b6ff26a60ee1c9f12e7eb3
Brunosilvestre00 <bruno.ap.silvestre@gmail.com>
2022-11-21 16:12:37 -0300

    add toggle switch

---
+++ src/components/ToggleSwitch/index.styled.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: auto;
+  text-align: center;
+`;
+
+export const Input = styled.input`
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+`;
+
+export const Mark = styled.div`
+  height: 0.7em;
+  width: 0.7em;
+  border-radius: 1em;
+  background: #fafafa;
+  -webkit-transition: all 300ms;
+  -moz-transition: all 300ms;
+  transition: all 300ms;
+`;
+
+export const Switch = styled.label<{ checked: boolean }>`
+  background: ${({ checked }) => (checked ? "#499F68" : "#9e9e9e")};
+  font-size: 1.5em;
+  height: 1em;
+  width: 2em;
+  border-radius: 1em;
+  align-items: center;
+  display: flex;
+  padding: 0 0.15em;
+  margin-bottom: 0;
+  cursor: pointer;
+  ${Input}:checked + ${Mark} {
+    -webkit-transform: translate3d(140%, 0, 0);
+    -moz-transform: translate3d(140%, 0, 0);
+    transform: translate3d(140%, 0, 0);
+  }
+`;
