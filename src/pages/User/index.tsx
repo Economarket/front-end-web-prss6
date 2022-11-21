3de5a8a38229ac7e51924345425bd6b6c84df888
MeiriBernardina <meiri.umc@gmail.com>
2022-11-21 17:42:58 -0300

    fix layout create user

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,16 +1,16 @@
-import ExternalAccessContainer from "../../templates/ExternalLayout";
+import ExternalAccessContainer from '../../templates/ExternalLayout';
 
-import * as S from "../styles";
-import InputText from "../../components/InputText";
-import registerUser from "../../assets/registerUser.png";
-import Button from "../../components/Button";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { schemaRegisterUser } from "../../utils/schema";
-import { useForm } from "react-hook-form";
-import InputPassword from "../../components/InputPassword";
-import PasswordRules from "../../components/PasswordRules";
-import { postUser } from "../../services/user";
-import { useNavigate } from "react-router-dom";
+import * as S from '../styles';
+import InputText from '../../components/InputText';
+import registerUser from '../../assets/registerUser.png';
+import Button from '../../components/Button';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { schemaRegisterUser } from '../../utils/schema';
+import { useForm } from 'react-hook-form';
+import InputPassword from '../../components/InputPassword';
+import PasswordRules from '../../components/PasswordRules';
+import { postUser } from '../../services/user';
+import { useNavigate } from 'react-router-dom';
 
 export default function User() {
   const navigate = useNavigate();
@@ -22,21 +22,21 @@ export default function User() {
     formState: { errors },
   } = useForm({
     defaultValues: {
-      name: "",
-      email: "",
-      password: "",
-      confirmPassword: "",
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
     },
     resolver: yupResolver(schemaRegisterUser),
   });
 
   const onSubmit = async (data: any) => {
     await postUser(data.name, data.password, data.email).then(() => {
-      navigate("/login");
+      navigate('/login');
     });
   };
   return (
-    <S.Wrapper>
+    <>
       <ExternalAccessContainer
         title="Vamos começar o cadastro?"
         subtitle="Preencha os campos abaixo para criar um novo cadastro."
@@ -44,39 +44,39 @@ export default function User() {
       >
         <S.Form onSubmit={handleSubmit(onSubmit)}>
           <InputText
-            {...register("name")}
+            {...register('name')}
             name="name"
             label="Nome"
             placeholder="Digite seu nome"
             errorMessage={errors.name?.message}
           />
           <InputText
-            {...register("email")}
+            {...register('email')}
             name="email"
             label="E-mail"
             placeholder="Digite seu e-mail"
             errorMessage={errors.email?.message}
           />
           <InputPassword
-            {...register("password")}
+            {...register('password')}
             name="password"
             label="Senha"
             placeholder="Digite uma senha"
             errorMessage={errors.password?.message}
           />
           <InputPassword
-            {...register("confirmPassword")}
+            {...register('confirmPassword')}
             name="confirmPassword"
             label="Confirme a senha"
             placeholder="Confirme a senha"
             errorMessage={errors.confirmPassword?.message}
           />
 
-          <PasswordRules password={watch("password")} />
+          <PasswordRules password={watch('password')} />
 
           <Button text="Cadastrar" onClick={handleSubmit(onSubmit)} />
         </S.Form>
       </ExternalAccessContainer>
-    </S.Wrapper>
+    </>
   );
 }
