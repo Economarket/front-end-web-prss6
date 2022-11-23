4eeabf6ebcee2b2d064f132958a9c627bbf53754
Meiri Bernardina <51059393+MeiriBernardina@users.noreply.github.com>
2022-11-23 08:38:58 -0300

    link botao apk (#58)

---
+++ src/assets/icons/androidLogo.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export default function IconAndroidLogo(
+  props: JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>,
+) {
+  return (
+    <svg
+      {...props}
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M15.375 15.75C15.9963 15.75 16.5 15.2463 16.5 14.625C16.5 14.0037 15.9963 13.5 15.375 13.5C14.7537 13.5 14.25 14.0037 14.25 14.625C14.25 15.2463 14.7537 15.75 15.375 15.75Z"
+        fill="#3B5249"
+      />
+      <path
+        d="M8.625 15.75C9.24632 15.75 9.75 15.2463 9.75 14.625C9.75 14.0037 9.24632 13.5 8.625 13.5C8.00368 13.5 7.5 14.0037 7.5 14.625C7.5 15.2463 8.00368 15.75 8.625 15.75Z"
+        fill="#3B5249"
+      />
+      <path
+        d="M2.25 18.0001V15.8532C2.25 10.4626 6.58125 6.01882 11.9625 6.00007C13.246 5.99514 14.5179 6.24368 15.7052 6.73146C16.8924 7.21923 17.9717 7.93663 18.881 8.84249C19.7904 9.74835 20.5119 10.8249 21.0043 12.0102C21.4966 13.1956 21.75 14.4665 21.75 15.7501V18.0001C21.75 18.199 21.671 18.3897 21.5303 18.5304C21.3897 18.6711 21.1989 18.7501 21 18.7501H3C2.80109 18.7501 2.61032 18.6711 2.46967 18.5304C2.32902 18.3897 2.25 18.199 2.25 18.0001Z"
+        stroke="#3B5249"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3 5.25L5.91562 8.16562"
+        stroke="#3B5249"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M21.0004 5.25L18.1035 8.14688"
+        stroke="#3B5249"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
