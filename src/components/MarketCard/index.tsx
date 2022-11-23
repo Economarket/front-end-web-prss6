800a0f79f7188b67fdc6113d934a0dca0e49d97f
AndrezaEmiliano <andreza.alamino@gmail.com>
2022-11-23 00:18:42 -0300

    fix images markets

--- src/components/MarketCard/index.tsx
+++ src/components/MarketCard/index.tsx
@@ -1,27 +1,36 @@
 import { markets_assets } from "../../assets/markets/markets";
-import { Market } from "../../services/models"
-import { Address, AddressContainer, Button, Container, InfoContainer, LogoContainer, Logo, Title } from "./index.styled";
+import { Market } from "../../services/models";
+import {
+  Address,
+  AddressContainer,
+  Button,
+  Container,
+  InfoContainer,
+  LogoContainer,
+  Logo,
+  Title,
+} from "./index.styled";
 
 interface MarketCardProps {
-    market: Market;
-    onClick: () => void;
+  market: Market;
+  onClick: () => void;
 }
 const MarketCard: React.FC<MarketCardProps> = ({ market, onClick }) => {
-    return (
-        <Container>
-            <LogoContainer>
-                <Logo alt={market.name} src={markets_assets[market.uuid]}/>
-            </LogoContainer>
-            <InfoContainer>
-                <Title>{market.name}</Title>
-                <AddressContainer>
-                    <Address>{`${market.address?.street}, ${market.address?.number}`}</Address>
-                    <Address>{`CEP: ${market.address?.cep}`}</Address>
-                </AddressContainer>
-                <Button onClick={onClick}>Ir para essa loja</Button>
-            </InfoContainer>
-        </Container>
-    );
+  return (
+    <Container>
+      <LogoContainer>
+        <Logo alt={market.name} src={markets_assets[market.name ?? ""]} />
+      </LogoContainer>
+      <InfoContainer>
+        <Title>{market.name}</Title>
+        <AddressContainer>
+          <Address>{`${market.address?.street}, ${market.address?.number}`}</Address>
+          <Address>{`CEP: ${market.address?.cep}`}</Address>
+        </AddressContainer>
+        <Button onClick={onClick}>Ir para essa loja</Button>
+      </InfoContainer>
+    </Container>
+  );
 };
 
 export default MarketCard;
